dbb1f771944197f558a25d11b29586e4a4abc632
Okarpets <karpetsoleksandrschool10@gmail.com>
2024-06-30 11:24:36 +0300

    Added all task 3.1 to new branch for this task

--- Lection 3/task 3.1/ReportApp/obj/Release/net6.0/ReportApp.AssemblyInfo.cs
+++ Lection 3/task 3.1/ReportApp/obj/Release/net6.0/ReportApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72e527e0f7b5f3afe980309f6e5a2f05e530947")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportApp")]
