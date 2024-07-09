e8d200045249991b3f257e6f504f1e3665f41902
Karpets Oleksandr <karpetsoleksandrschool10@gmail.com>
2024-07-09 22:54:45 +0300

    Simplified line 29

---
+++ Lection 2/task 2.2/task 2.2/Models/GameSystem.cs
@@ -0,0 +1,90 @@
+﻿namespace Lection_2_task_2._2.Models;
+
+public class GameSystem
+{
+    private readonly byte PageSize = 5;
+    public Game GetById(List<Game> Games, decimal OrderedId)
+    {
+        return Games.FirstOrDefault(x => x.Id == OrderedId);
+    }
+
+    public List<string> GetListByPriceRange(List<Game> Games, decimal OrderPriceMin, decimal OrderPriceMax)
+    {
+        return Games.Where(Game => Game.Price <= OrderPriceMax && Game.Price >= OrderPriceMin).Select(Game => Game.Name).ToList();
+    }
+
+    public IEnumerable<Genre> GetListOfGenresByGame(List<Game> Games, string GameName)
+    {
+        if (Games.Any(x => x.Name == GameName))
+        {
+            return Games.First(x => x.Name == GameName).Genres;
+        }
+        return null;
+    }
+
+    public List<string> GetUniqueCategoriesFromGameList(List<Game> Games)
+    {
+        List<string> Result = new List<string>();
+        Result.AddRange(Games.Select(g => g.Category));
+        return Result.Where(s => Result.Count(x => x == s) == 1).ToList();
+    }
+
+    public List<string> GetFilterGamesByCategoryAndGenres(List<Game> Games, List<string> genres, string category)
+    {
+        return Games.Where(Game => Game.Category == category && !genres.Except(Game.Genres.Select(g => g.Name)).Any()).Select(Game => Game.Name).ToList();
+    }
+
+    public int GetPageCount(List<string> Games)
+    {
+        return (int)Math.Ceiling((double)Games.Count / PageSize);
+    }
+
+    public void PageHandler(List<string> Data, string Type)
+    {
+        var PageCount = GetPageCount(Data);
+
+        while (true)
+        {
+            Console.WriteLine($"Please enter a page (All pages: {PageCount})");
+            int IndexOfPage = Convert.ToInt32(Console.ReadLine());
+            var Paginaion = Pagination(Data, IndexOfPage);
+
+            if (Paginaion.Count == 0)
+            {
+                Console.WriteLine("Page with this index range doesn't exist");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(Type == "Price" ? "GAMES" : "GENRES");
+                foreach (string Value in Paginaion)
+                {
+                    Console.WriteLine(Value);
+                }
+            }
+            Console.WriteLine("Do you want to stop GLBP command? (Y/ )");
+            char Order = Convert.ToChar(Console.ReadLine());
+            if (Order == 'Y' || Order == 'y')
+            {
+                return;
+            }
+        }
+    }
+
+    public List<string> Pagination(List<string> Games, int PageNumber)
+    {
+        return Games.Skip((PageNumber - 1) * PageSize).Take(PageSize).ToList();
+    }
+
+    public string Commands()
+    {
+        string Command =
+        "* Q - exit\n" +
+        "* GBI - Get a game by id\n" +
+        "* GLBP - Get list of games by price range\n" +
+        "* GLBG - Get list of genres by game\n" +
+        "* GUC - Get unique categories from game list\n" +
+        "* GG - Get filter games by genres\n";
+        return Command;
+    }
+}
---
+++ Lection 2/task 2.2/task 2.2/Program.cs
@@ -0,0 +1,112 @@
+﻿using Lection_2_task_2._2.Models;
+
+namespace Program;
+
+internal class Program
+{
+    static public void Main(string[] args)
+    {
+        GameSystem GameSystem = new GameSystem();
+        while (true)
+        {
+            Console.WriteLine("Please, enter a command: ");
+            string Command = Convert.ToString(Console.ReadLine());
+            switch (Command)
+            {
+                case "-help":
+                    Console.WriteLine(GameSystem.Commands());
+                    break;
+
+                case "GBI":
+                    Console.WriteLine("Enter an id of the game:");
+                    decimal Id = Convert.ToDecimal(Console.ReadLine());
+                    var GameById = GameSystem.GetById(GameList.ListOfGames, Id);
+                    if (GameById != null)
+                    {
+                        Console.WriteLine($"\tSEARCH RESULT\nName: {GameById.Name}\nPrice: {GameById.Price}\nCategory: {GameById.Category}\n\tGENRES");
+                        foreach (Genre Genre in GameById.Genres)
+                        {
+                            Console.WriteLine(Genre.Name);
+                        }
+                        break;
+                    }
+                    Console.WriteLine("Game with this ID doesn't exist");
+                    break;
+
+                case "GLBP":
+                    Console.WriteLine("Enter min price of the game:");
+                    decimal MinPrice = Convert.ToDecimal(Console.ReadLine());
+                    Console.WriteLine("Enter max price of the game:");
+                    decimal MaxPrice = Convert.ToDecimal(Console.ReadLine());
+                    var GamesByPrice = GameSystem.GetListByPriceRange(GameList.ListOfGames, MinPrice, MaxPrice);
+                    if (GamesByPrice == null)
+                    {
+                        Console.WriteLine("Game with this price range doesn't exist");
+                        break;
+                    }
+                    GameSystem.PageHandler(GamesByPrice, "Price");
+                    break;
+
+                case "GLBG":
+                    Console.WriteLine("Enter name of the game:");
+                    string NameOfGame = Convert.ToString(Console.ReadLine());
+                    var Ganres = GameSystem.GetListOfGenresByGame(GameList.ListOfGames, NameOfGame);
+                    if (Ganres == null)
+                    {
+                        Console.WriteLine("This game doesn't exists");
+                        break;
+                    }
+                    foreach (Genre Genre in Ganres)
+                    {
+                        Console.WriteLine(Genre.Name);
+                    }
+                    break;
+
+                case "GUC":
+                    var Unique = GameSystem.GetUniqueCategoriesFromGameList(GameList.ListOfGames);
+                    if (Unique.Count == 0)
+                    {
+                        Console.WriteLine("Unique category doesn't exist");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unique category:");
+                        foreach (string Genre in Unique)
+                        {
+                            Console.WriteLine($"{Genre}");
+                        }
+                    }
+
+                    break;
+
+                case "Q":
+                    Environment.Exit(1);
+                    break;
+
+                case "GG":
+                    List<string> GenresToFilter = new List<string>();
+                    Console.WriteLine("Enter the genres to filtration:");
+                    while (true)
+                    {
+                        string GanreToFiltration = Convert.ToString(Console.ReadLine());
+                        GenresToFilter.Add(GanreToFiltration);
+                        Console.WriteLine("Stop entering (Y/ )");
+                        char OrderGenres = Convert.ToChar(Console.ReadLine());
+                        if (OrderGenres == 'Y' || OrderGenres == 'y')
+                        {
+                            break;
+                        }
+                    }
+                    Console.WriteLine("Enter the category to filtration:");
+                    string CategoryToFiltration = Convert.ToString(Console.ReadLine());
+                    var GamesByFilter = GameSystem.GetFilterGamesByCategoryAndGenres(GameList.ListOfGames, GenresToFilter, CategoryToFiltration);
+                    GameSystem.PageHandler(GamesByFilter, "Genres");
+                    break;
+
+                default:
+                    Console.WriteLine("It is a wrong command, please, use \"-help\"");
+                    break;
+            }
+        }
+    }
+}
